40c1f36d46fa4d6803f4db8382cd765eabc455eb
Sunny <sj7821054@gamil.com>
2024-04-13 13:19:56 +0530

    fixed it

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 async function start() {
     const weatherPromise =  await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast") ;
     const weatherData = await weatherPromise.json()
-    const ourTemperature = 
-console.log(ourTemperature) 
-
+    const ourTemperature = weatherData.properties.periods[0].temperature
+    console.log(ourTemperature) 
 }
-
 start()
 
